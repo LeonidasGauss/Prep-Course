cdf88b9086f24aed2ca26095be4af5d1f6f7b2f2
LeonidasGauss <leocastillo810@gmail.com>
2022-08-26 18:25:12 -0500

    Tercer commit

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -4,7 +4,7 @@
 const nuevaString = 'hola';
 
 // Crea una variable numérica, puede ser cualquier número:
-const nuevoNum = 'hola de nuevo';
+const nuevoNum = 'No c que esta pasando';
 
 // Crea una variable booleana:
 const nuevoBool = null;
